aa55ec8939b56baea7e56ce755f84a7cfc19cbbc
philmaweb <philmaweb@gmail.com>
2017-02-23 15:18:42 +0100

    facilitated and made call with git more uniform using clone --recursive git:// instead of https and ssh

--- setup.sh
+++ setup.sh
@@ -15,8 +15,8 @@ cp dotsetup/.bashrc ~ && \
 cp -f dotsetup/.gitconfig ~ && \
 rm -rf ~/.vim/bundle && \
 rm -rf ~/.vim/colors && \
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
-git clone git@github.com:altercation/vim-colors-solarized.git ./solarized && \
+git clone --recursive git://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
+git clone --recursive git://github.com/altercation/vim-colors-solarized.git ./solarized && \
 mkdir ~/.vim/colors && \
 mv ./solarized/colors/solarized.vim ~/.vim/colors/ && \
 rm -rf ./solarized
