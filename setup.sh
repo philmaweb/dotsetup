403a3bb306c93c632b0903cbdf5abdfea413e124
Philipp Weber <philmaweb@gmail.com>
2018-10-22 21:27:33 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,17 +4,17 @@ echo "Starting Setup" && \
 if [ ! -f ~/.vimrc ]; then
   touch ~/.vimrc
 fi
-if [ ! -f ~/.gitconfig ]; then
-  touch ~/.gitconfig
-fi
+#if [ ! -f ~/.gitconfig ]; then
+#  touch ~/.gitconfig
+#fi
 mv -f ~/.vimrc ~/.vimrc.old_beforedotsetup
-mv  ~/.bashrc ~/.bashrc.old_beforedotsetup
-cp -f ~/.gitconfig ~/.gitconfig.old_beforedotsetup && \
+#mv  ~/.bashrc ~/.bashrc.old_beforedotsetup
+#cp -f ~/.gitconfig ~/.gitconfig.old_beforedotsetup && \
 cp -f dotsetup/.vimrc ~ && \
-cp dotsetup/.bashrc ~ && \
-cp -f dotsetup/.gitconfig ~ && \
-rm -rf ~/.vim/bundle && \
-rm -rf ~/.vim/colors && \
+#cp dotsetup/.bashrc ~ && \
+#cp -f dotsetup/.gitconfig ~ && \
+#rm -rf ~/.vim/bundle && \
+#rm -rf ~/.vim/colors && \
 git clone --recursive git://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
 git clone --recursive git://github.com/altercation/vim-colors-solarized.git ./solarized && \
 mkdir ~/.vim/colors && \
