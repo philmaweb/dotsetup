beb3c11f4eb851c3a0e570b6413d1cde65c4ecba
Philipp Weber <philmaweb@gmail.com>
2018-12-10 19:45:54 -0500

    Delete reinstall_all_python.sh

--- reinstall_all_python.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Many hours of research after a failed manual install of python later, this question got the solution http://askubuntu.com/questions/270011/broken-python-2-7-after-manually-building-and-installing-python-2-6
-# let's reinstall everything with python
-for pkg in `dpkg --get-selections | egrep -v 'deinstall' | egrep python | awk '{print $1}'`; do  apt-get -y --force-yes install --reinstall $pkg ; done
-echo "Thank you for reinstalling your native Python!"
